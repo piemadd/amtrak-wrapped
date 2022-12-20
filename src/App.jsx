8ac06f0dbc2f133f1e4db5a859225763e6100106
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-12-19 22:56:25 -0600

    inputs working

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ import stations from "./data/stations";
 import "./App.css";
 
 export default function App() {
+  const calculateDistanceBetweenCoordinates = (lat1, lon1, lat2, lon2) => {
+    const R = 6371e3; // metres
+    const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+    const φ2 = (lat2 * Math.PI) / 180;
+    const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+    const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+    const a =
+      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c; // in metres
+    return d;
+  };
+
   const [inputFields, setInputFields] = useState([
     { train: "", start: "", stop: "" },
   ]);
@@ -16,14 +32,29 @@ export default function App() {
     console.log(inputFields);
   };
 
+  const deleteFields = (index) => {
+    let data = [...inputFields];
+    data.splice(index, 1);
+
+    if (data.length === 0) data.push({ train: "", start: "", stop: "" });
+    setInputFields(data);
+  };
+
+  const copyFields = (index) => {
+    let data = [...inputFields];
+    data.push(data[index]);
+    setInputFields(data);
+  };
+
   const addFields = () => {
     let newfield = { train: "", start: "", stop: "" };
-
     setInputFields([...inputFields, newfield]);
   };
 
   return (
     <main>
+      <h1>Traked</h1>
+      <p id="slogan">How has your year <i>Traked</i>?</p>
       <section>
           <datalist id="trainList">
             {routes.map((route) => {
@@ -69,6 +100,10 @@ export default function App() {
                   value={input.stop}
                   onChange={(event) => handleFormChange(index, event)}
                 />
+                <br />
+
+                <button onClick={() => deleteFields(index)}>Delete</button>&nbsp;
+                <button onClick={() => copyFields(index)}>Copy</button>
               </div>
             );
           })}
